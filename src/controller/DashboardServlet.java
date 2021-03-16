64107c145c5104cb86a24778252fab07d0580395
Neix20 <66442152+Neix20@users.noreply.github.com>
2021-03-16 12:15:17 +0800

    Completed Dashboard Logic
    Havent Complete GUI

--- src/controller/DashboardServlet.java
+++ src/controller/DashboardServlet.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,6 +31,7 @@ import session_bean.OrderDetailSessionBeanLocal;
 import session_bean.OrderSessionBeanLocal;
 import session_bean.ProductSessionBeanLocal;
 import session_bean.ProductlineSessionBeanLocal;
+import utility.html_generator;
 
 /**
  * Servlet implementation class DashboardServlet
@@ -94,18 +97,115 @@ public class DashboardServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		//Get Weekly Order
-		//Use Stream Filter
+		//Use Stream Filter Reduce to sum up all the buyPrice, Sell Price, Total Sales Revenue
+		List<DashboardOrder> tmp = new ArrayList<DashboardOrder>();
+		ArrayList<Integer> totalOrderWeekList = new ArrayList<Integer>(), totalProductWeekList = new ArrayList<Integer>();
+		ArrayList<BigDecimal> totalBuyPriceWeekList = new ArrayList<BigDecimal>(), totalMsrpWeekList = new ArrayList<BigDecimal>(), totalSalesWeekList = new ArrayList<BigDecimal>();
+		BigDecimal buyPriceWeekSum, msrpWeekSum, salesWeekSum;
+		ShoppingCart weekCartList;
+		int weekNum = 1;
+		for(DashboardOrder dbo : orderMonthList) {
+			if(weekNum == dbo.getWeekRange()) {
+				tmp.add(dbo);
+			}else {
+				//Execute Last List
+				weekCartList = getShoppingCartList(tmp);
+				weekCartList.countTotalPrice();
+				
+				//Total Number of Orders for the Week
+				totalOrderWeekList.add(tmp.size());
+				
+				//Total Number of Products
+				totalProductWeekList.add(weekCartList.count());
+				
+				//Total Buy Price for the week
+				buyPriceWeekSum = weekCartList.getList().stream().map(y -> y.getBuyprice().multiply(new BigDecimal(y.getQuantity()))).reduce(BigDecimal.ZERO, BigDecimal::add);
+				totalBuyPriceWeekList.add(buyPriceWeekSum);
+				
+				//Total MSRP for the week
+				msrpWeekSum = weekCartList.getTotalPrice();
+				totalMsrpWeekList.add(msrpWeekSum);
+				
+				//Total Sales Revenue for the week
+				salesWeekSum = msrpWeekSum.subtract(buyPriceWeekSum);
+				totalSalesWeekList.add(salesWeekSum);
+				
+				weekNum = dbo.getWeekRange();
+				
+				//Init new List
+				tmp = new ArrayList<DashboardOrder>();
+				tmp.add(dbo);
+			}
+		}
+		//Execute Last List
+		weekCartList = getShoppingCartList(tmp);
+		weekCartList.countTotalPrice();
+		
+		//Total Number of Orders for the Week
+		totalOrderWeekList.add(tmp.size());
+		
+		//Total Number of Products
+		totalProductWeekList.add(weekCartList.count());
+		
+		//Total Buy Price for the week
+		buyPriceWeekSum = weekCartList.getList().stream().map(y -> y.getBuyprice().multiply(new BigDecimal(y.getQuantity()))).reduce(BigDecimal.ZERO, BigDecimal::add);
+		totalBuyPriceWeekList.add(buyPriceWeekSum);
+		
+		//Total MSRP for the week
+		msrpWeekSum = weekCartList.getTotalPrice();
+		totalMsrpWeekList.add(msrpWeekSum);
+		
+		//Total Sales Revenue for the week
+		salesWeekSum = msrpWeekSum.subtract(buyPriceWeekSum);
+		totalSalesWeekList.add(salesWeekSum);
+		
+		out.println("<h1>Total Orders Week</h1>");
+		for(int i : totalOrderWeekList) {
+			out.println(html_generator.p_tag("Total Order Week: " + i));
+		}
+		
+		out.println("<h1>Total Products Week</h1>");
+		for(int i : totalProductWeekList) {
+			out.println(html_generator.p_tag("Total Products Week: " + i));
+		}
+		
+		out.println("<h1>Total Buy Price Week: </h1>");
+		for(BigDecimal bd : totalBuyPriceWeekList) {
+			out.println(html_generator.p_tag("Total Buy Price: " + bd.setScale(2, RoundingMode.HALF_UP)));
+		}
+		
+		out.println("<h1>Total MSRP Week: </h1>");
+		for(BigDecimal bd : totalMsrpWeekList) {
+			out.println(html_generator.p_tag("Total MSRP Price: " + bd.setScale(2, RoundingMode.HALF_UP)));
+		}
+		
+		out.println("<h1>Total Sales Revenue Week: </h1>");
+		for(BigDecimal bd : totalSalesWeekList) {
+			out.println(html_generator.p_tag("Total Sales Revenue: " + bd.setScale(2, RoundingMode.HALF_UP)));
+		}
+		
 
-		ShoppingCart scList = getShoppingCartList(orderList);
+		ShoppingCart scList = getShoppingCartList(orderMonthList);
 		List<ShoppingCartItem> tmpList = scList.getList().stream().sorted(Comparator.comparingInt(ShoppingCartItem::getQuantity)).collect(Collectors.toList());
 		Collections.reverse(tmpList);
 		
-		for(ShoppingCartItem sc : tmpList) {
-			out.println("<p>" + sc.getProductname() + " " + sc.getQuantity() + "</p>");
-		}
-
+		int orderShipped = orderMonthList.stream().filter(y -> y.getStatus().equals("Shipped")).collect(Collectors.toList()).size();
+		int orderHaventShipped = orderMonthList.size() - orderShipped;
+		
+		out.println(html_generator.p_tag("Total Quantity: " + scList.count()));
+		out.println(html_generator.p_tag("Total Order Shipped: " + orderShipped));
+		out.println(html_generator.p_tag("Total Order not shipped: " + orderHaventShipped));
+		
+		out.println(html_generator.p_tag("Mum"));
+		
+		
+		BigDecimal totalBuyPriceMonth, totalSalesMonth, totalMsrpMonth = scList.getTotalPrice();
+		totalBuyPriceMonth = scList.getList().stream().map(y -> y.getBuyprice().multiply(new BigDecimal(y.getQuantity()))).reduce(BigDecimal.ZERO, BigDecimal::add);
+		totalSalesMonth = totalMsrpMonth.subtract(totalBuyPriceMonth);
+		
+		out.println(html_generator.p_tag("Total Sales Revenue: " + totalSalesMonth.doubleValue()));
+		
 		HashMap<String, Integer> productlineHashMap = getProductlineHashMap(scList);
-
 		for (String s : productlineHashMap.keySet()) {
 			out.println("<p>" + s + " " + productlineHashMap.get(s) + "</p>");
 		}
@@ -127,11 +227,13 @@ public class DashboardServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
-	public ShoppingCart getShoppingCartList(List<Order> orderList) {
+	
+	//Convert this to DashBoardOrder List Parameter and put it in Utilities
+	public ShoppingCart getShoppingCartList(List<DashboardOrder> dboList) {
 		List<Orderdetail> orderdetailList = new ArrayList<Orderdetail>();
 		ShoppingCart scList = new ShoppingCart();
-		for (Order o : orderList) {
-			List<Orderdetail> tmp = orderDetailBean.getAllOrderDetails(o.getOrdernumber());
+		for (DashboardOrder dbo : dboList) {
+			List<Orderdetail> tmp = orderDetailBean.getAllOrderDetails(dbo.getOrdernumber());
 			orderdetailList.addAll(tmp);
 		}
 
@@ -145,18 +247,15 @@ public class DashboardServlet extends HttpServlet {
 
 		return scList;
 	}
-
+	
+	//Key => Productline, value => Count
 	public HashMap<String, Integer> getProductlineHashMap(ShoppingCart scList) {
 		List<Productline> productlineList = productlineBean.getAllProductline();
 		HashMap<String, Integer> productlineHashMap = new HashMap<String, Integer>();
-		for (Productline pl : productlineList)
-			productlineHashMap.put(pl.getProductline(), 0);
-
-		for (ShoppingCartItem sc : scList.getList()) {
-			int x = productlineHashMap.get(sc.getProductline());
-			for (int i = 0; i < sc.getQuantity(); i++)
-				x++;
-			productlineHashMap.put(sc.getProductline(), x);
+		
+		for (Productline pl : productlineList) {
+			int quantity = scList.getList().stream().filter(x -> x.getProductline().equals(pl.getProductline())).reduce(0, (sum, y) -> sum + y.getQuantity(), Integer::sum);
+			productlineHashMap.put(pl.getProductline(), quantity);
 		}
 
 		return productlineHashMap;
